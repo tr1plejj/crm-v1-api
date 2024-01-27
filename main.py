f96414aeb57bd081f3d254ea3b059b5a4cf7a1fa
avg_prog_enj <01iVeR@inbox.ru>
2024-01-27 15:47:36 +0500

    created api server

--- main.py
+++ main.py
@@ -1,14 +1,78 @@
 from fastapi import FastAPI
+import uvicorn
+import psycopg2
+# from config import user, host, password, db_name
+host = 'localhost'
+user = 'postgres'
+password = 'Zxc1259663oliver'
+db_name = 'products_data'
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get('/{name}/{price}/{description}')
+def add_in_db(name: str, price: str, description: str):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        with connection.cursor() as cursor:
+            cursor.execute(f"insert into prod_data(name, price, description) values ('{name}', '{price}', '{description}')")
+            cursor.execute("select id from prod_data order by id desc limit 1")
+            last_id = cursor.fetchone()
+        connection.commit()
+        return last_id
+    except Exception as _ex:
+        print('[INFO]', _ex)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    finally:
+        if psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+        ):
+            psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            ).close()
 
-print(read_item(1))
+@app.get('/{id}')
+def take_from_db(id: int):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        with connection.cursor() as cursor:
+            cursor.execute(f"select name, price, description from prod_data where id = {id}")
+            about_prod = cursor.fetchall()
+        connection.commit()
+        return about_prod
+    except Exception as _ex:
+        print('[INFO]', _ex)
+
+    finally:
+        if psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+        ):
+            psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            ).close()
+
+if __name__ == '__main__':
+    uvicorn.run(app)
 
--- req.py
+++ req.py
@@ -1,3 +1,4 @@
 import requests
-response = requests.get('http://127.0.0.1:8000/items/1')
+response = requests.get('http://127.0.0.1:8000/3').json()
+response = response[0][1]
 print(response)
