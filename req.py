70dfe49df15b1cf915f09f35f5de3876d60570c9
avg_prog_enj <01iVeR@inbox.ru>
2024-03-03 18:45:57 +0500

    version for school project

---
+++ req.py
@@ -0,0 +1,11 @@
+import asyncio
+
+from models import async_engine, Base
+
+
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(create_tables())
